aaf6d8d86b21ea0e2834109ec45aac978c9208c5
Ahmmed Sakib Noman <nomanahmmed0@gmail.com>
2021-09-14 13:39:41 +0600

    Going back to fedora

---
+++ lab_one/a.cpp
@@ -0,0 +1,62 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+#define F first
+#define S second
+#define eb emplace_back
+
+using ll = long long int;
+
+void solve ()
+{
+}
+
+int factorial ( int n )
+{
+	if ( n < 2 ) return 1;
+	else return n * factorial ( n - 1 );
+}
+
+int main ()
+{
+	int n;
+	printf ( "Enter the value of n : " );
+	scanf ( "%d", &n );
+	assert ( n > -1 );
+	int ans = factorial ( n );
+	printf ( "Factorial of %d is : %d\n", n, ans );
+
+
+	return 0;
+}
+
---
+++ lab_one/b.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdbool.h>
+
+int fib ( int n )
+{
+	if ( n == 0 || n == 1 ) return n;
+	else if ( n > 1 ) return fib ( n - 2 ) + fib ( n - 1 );
+
+	// making sure the program is executing properly
+	// going to show runtime error if something bad happens
+	assert ( false );
+}
+
+int main ()
+{
+	int n;
+	printf ( "Enter until which term you want to see the series : " );
+	scanf ( "%d", &n );
+
+	// making sure the input is not negative or zero
+	// going to show runtime error
+	assert ( n > 0 );
+
+	if ( n > 40 ) {
+		printf ( "This algorithm is not efficient enough" );
+		printf ( " to calculate more than 40 terms." );
+		puts("\n");
+		fflush ( stdout );
+
+		// exiting the program with runtime error
+		assert ( false );
+	}
+
+	printf ( "The fibonacci series upto %d term is :\n" );
+	for ( int i = 0 ; i < n ; ++i ) {
+		printf ( "%d ", fib ( i ) );
+	}
+	puts("\n");
+
+
+	return 0;
+}
+
